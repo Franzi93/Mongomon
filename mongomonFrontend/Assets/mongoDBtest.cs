1fdd226e52c32f01d796552ba782eb9fafbd5a10
Franziska Neu <fn@demodern.de>
2019-09-25 16:32:13 +0200

    init commit

---
+++ mongomonFrontend/Assets/mongoDBtest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Http;
+
+public class mongoDBtest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var values = new Dictionary<string, string>
+{
+{ "thing1", "hello" },
+{ "thing2", "world" }
+};
+
+        var content = new FormUrlEncodedContent(values);
+        HttpClient client = new HttpClient();
+        var response = client.PostAsync("http://localhost:8080/game/", content);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
